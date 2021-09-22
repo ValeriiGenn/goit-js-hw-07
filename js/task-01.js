0030d4e72b53bb83fe83f8c591190ae38499de58
ValeriiGenn <pvg_erku@ukr.net>
2021-09-22 21:24:26 +0300

    2100

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,12 @@
-const ulEl = document.querySelector("#categories");
+const categoryListRef = document.querySelectorAll("li.item");
+console.log(`В списке ${categoryListRef.length} категории.`);
 
-const itemEl = document.querySelector(".item");
-
-//itemEl.forEach(el=> console.log (el.length));
-
-const titleEl = document.querySelectorAll("h2");
-titleEl.forEach((element) => console.log(element.textContent));
-
-const childUl = itemEl.lastElementChild;
-
-const childItemUl = childUl.children.length;
-
-console.log(childItemUl);
+const categoryRef = document.querySelectorAll("li.item");
+categoryRef.forEach((category) =>
+    console.log(
+        "Категория :",
+        category.firstElementChild.textContent,
+        " , количество элементов:",
+        category.lastElementChild.children.length
+    )
+);
